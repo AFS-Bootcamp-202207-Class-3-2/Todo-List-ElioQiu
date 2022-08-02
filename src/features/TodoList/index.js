6b189f07f683315c255433df6c443893d0ffd6ba
blank-1024 <13380497335@163.com>
2022-08-02 14:02:22 +0800

    feat: add redux in app

---
+++ src/features/TodoList/index.js
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+import './index.css'
+import TodoGroup from "../TodoGroup";
+import TodoGenerator from "../todos/TodoGenerator";
+
+export default function TodoList() {
+    const [todoList, setTodoList] = useState([])
+
+    const addTodo = (todoObj) => {
+        const newTodos = [todoObj,...todoList]
+        setTodoList(newTodos)
+    }
+
+    return (
+        <div>
+            <span>Todo List</span>
+            <TodoGroup todoList = {todoList} />
+            <TodoGenerator addTodo = {addTodo} />
+        </div>
+    )
+}
