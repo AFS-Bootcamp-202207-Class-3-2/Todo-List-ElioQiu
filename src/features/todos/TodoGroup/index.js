34a6d43578498c523c2be00dc9a22f483108c5b4
blank1024 <13380497335@163.com>
2022-08-04 00:04:58 +0800

    feat: add Card to build TodoList Page

--- src/features/todos/TodoGroup/index.js
+++ src/features/todos/TodoGroup/index.js
@@ -1,12 +1,24 @@
 import TodoItem from "../TodoItem";
+import { Card, Col, Row } from 'antd';
+import React from "react";
 
 export default function TodoGroup(props) {
   const { todos } = props;
   return (
     <div>
-      {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={todo} />;
-      })}
+        <div className="site-card-wrapper">
+              <Row gutter={[16, 16]}>
+                {
+                  todos.map((todo) =>
+                      <Col span={8} key={todo.id}>
+                        <Card title={todo.done?"Done":"To be done"} bordered={true} hoverable={true}>
+                            <TodoItem key={todo.id} todo={todo} />
+                        </Card>
+                      </Col>
+                  )
+                }
+              </Row>
+            </div>
     </div>
   );
 }
