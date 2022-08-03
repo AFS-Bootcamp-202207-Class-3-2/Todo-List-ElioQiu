7fe6ee1d4ed6858fc703eadbc56936f165ffc370
blank1024 <13380497335@163.com>
2022-08-03 21:56:04 +0800

    feat: add not found image

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Image } from 'antd';
+import ImgSrc from '../../assets/404.jpg'
+import './index.css'
+import {useNavigate} from "react-router-dom";
 
 export default function NotFoundPage() {
+    const navigate = useNavigate()
+    useEffect(() => {
+        setTimeout(() =>{
+            navigate('/')
+        }, 5000)
+    }, [])
     return (
-        <div>
-            <h1>NotFoundPage</h1>
+        <div className="MyPage">
+            <Image src={ImgSrc} />
+            <br/><br/><br/>
+            <span><b>该路径不存在或资源已被删除，5s后自动跳转回首页。。。。。。</b></span>
         </div>
     )
 }
